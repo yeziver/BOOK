ae64440f5191861ba7615aed2b697759d63654f1
Yuting Liu <83076528+yeziver@users.noreply.github.com>
2023-05-20 16:13:12 -0400

    Update memoization.cpp

--- memoization.cpp
+++ memoization.cpp
@@ -1,3 +1,11 @@
+//How does dynamic programming work?
+//*The programs use extra memory to store the solution to sub problems and avoid recomputation.
+//TOP-DOWN DP:
+//  *If a certain state has been computed before, simply return the value from the DP memo table
+//  *If the state has not been computed, perform the computation as per normal and then store the comped value in the DP memo table and return it
+//BOTTOM-DOWN DP:
+//  *Sorts subproblems by input size and solves them iteratively from smallest to largest.
+
 //UVa 11450-Wedding Shopping
 //The abridged problem statement: Given different options for each garment (e.g. 3 shirt models, 2 belt models, 4 shoe models, . . . ) 
 //and a certain limited budget, our task is to buy one model of each garment. We cannot spend more money than the given budget, 
@@ -6,10 +14,6 @@
 //The input consists of two integers 1 ≤ M ≤ 200 and 1 ≤ C ≤ 20, where M is the budget and C is the number of garments that you have to buy, 
 //followed by some information about the C garments. For the garment g ∈ [0..C-1], we will receive an integer 1 ≤ K ≤ 20 which indicates the 
 //number of different models there are for that garment g, followed by K integers indicating the price of each model ∈ [1..K] of that garment g.
-
-//TOP-DOWN DP solution w/ memoization:
-//  *If a certain state has been computed before, simply return the value from the DP memo table
-//  *If the state has not been computed, perform the computation as per normal and then store the comped value in the DP memo table and return it
 
 //ABRIDGED SOLUTION:
 
